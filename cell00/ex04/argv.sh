95be2fda33d01ea667723363721425ba37a33fdc
Nanon Singhasurasak <nanon2546@yahoo.com>
2025-02-07 00:01:03 +0700

    Add files via upload

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# Check if no arguments are provided.
 if [ $# -eq 0 ]; then
   echo "No arguments supplied"
   exit 0
 fi
 
-# Loop over the first three arguments only.
 count=0
 for arg in "$@"; do
   echo "$arg"
   count=$((count + 1))
 
-  # Stop after printing three arguments.
   if [ $count -ge 3 ]; then
     break
   fi
